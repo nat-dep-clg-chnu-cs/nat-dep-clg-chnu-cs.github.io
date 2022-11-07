5ef3b8711369d7030ee01f4de8c4649b3f49f549
Природниче відділення ВСП "Фаховий Коледж ЧНУ <102409039+nat-dep-clg-chnu@users.noreply.github.com>
2022-11-07 12:18:08 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,14 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'preamble': r'''
+\usepackage[utf8]{inputenc}	
+''',
+    'babel': r'''
+    \usepackage[ukrainian]{babel}
+    ''',
+}
+latex_show_urls = 'footnote'
